e9ee2532e516206fbf9e48d79331ae5b26fbaf1d
Zhukova Olya <zhukova.o.m@yandex.ru>
2020-02-28 14:56:09 +0500

    Merge with kotur-courses

--- tasks/src/4.2.CreditCardInput/index.js
+++ tasks/src/4.2.CreditCardInput/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import PropTypes from 'prop-types';
-import 'regenerator-runtime/runtime';
+import 'regenerator-runtime';
 import './styles.css';
 import Api from './Api';
 import CreditCardNumber from './CreditCardNumber';
